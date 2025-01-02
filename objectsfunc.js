94394814fc87989f7083e9621d993efb916f797e
ksmbhv <ksupadhyaya27@gmail.com>
2025-01-02 11:55:40 +0530

    your name is jogn cenaaaaaaaaaaaaa

---
+++ objectsfunc.js
@@ -0,0 +1,14 @@
+/* let user = {
+    name: "kito",
+    age: 19
+}
+
+console.log(user.age);
+console.log(user["age"]); */
+
+let user = {
+    username: "lol cena",
+    age: 40
+}
+
+console.log("Your namne is: " + user.username + " and your age is: " + user.age);
